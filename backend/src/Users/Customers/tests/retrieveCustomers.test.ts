1cf6fe2118986d96ff2b4d80ce11fc7ec880072a
mateus-coder1 <letsgood56@gmail.com>
2023-01-06 01:15:02 -0300

    :white_check_mark: Tests: TASK-016 Add Retrieve Customers Test

--- backend/src/Users/Customers/tests/retrieveCustomers.test.ts
+++ backend/src/Users/Customers/tests/retrieveCustomers.test.ts
@@ -1,8 +1,17 @@
-import { describe, expect, test } from '@jest/globals'
+import { describe, expect, it, beforeAll } from '@jest/globals'
+import { Axios } from '../../../base/providers/Axios'
 import { retrieveCustomers } from '../services/retrieveCustomers'
+import dotenv from 'dotenv'
 
 describe('Customers SubModule', () => {
-  test('Retrieve Customers', () => {
-    expect(retrieveCustomers().length).toBe(2)
+  beforeAll(() => {
+    dotenv.config()
+  })
+  it('Can Retrieve Customers', async () => {
+    const customersApi = new Axios({
+      baseURL: process.env.CUSTOMERS_API_URL,
+    })
+    const customers = await retrieveCustomers(customersApi)
+    expect(customers.length).toBe(10)
   })
 })
