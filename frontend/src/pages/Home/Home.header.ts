85051362fa099caef1f6f881054008029c85e02c
mateus-coder1 <letsgood56@gmail.com>
2023-01-17 11:23:58 -0300

    :sparkles: Feat: TASK-025 Add HomePage

---
+++ frontend/src/pages/Home/Home.header.ts
@@ -0,0 +1,4 @@
+import styles from './Home.module.css'
+import CustomersList from '../../components/organisms/CustomersList/CustomersList'
+
+export { CustomersList, styles }
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import * as h from './Home.header'
+
+export default function Home() {
+  return (
+    <main className={h.styles.Home}>
+      <h.CustomersList />
+    </main>
+  )
+}
